cfdac5db85c1e911acb3361fbb3d26a14a03f8da
eliasmedeiros898 <102063068+eliasmedeiros898@users.noreply.github.com>
2022-05-27 18:59:19 -0300

    Add files via upload

---
+++ words_backup.py
@@ -0,0 +1,4 @@
+words_backup = ["pesquisa","natureza","objetivos","procedimentos","primaria","secundaria","terciaria",
+"exploratoria","descritiva","explicativa","design","bibliografica","documental","experimental","levantamento",
+"etnografica","ciencia","tecnologia","computacao","hipotese","empirica","formal","teoria","wazlawick","raul","metodos",
+"pesquisador","ideias","algoritmo","prototipo","processo"]
